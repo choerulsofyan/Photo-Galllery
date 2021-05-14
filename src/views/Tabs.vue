b0b107bb3631a848380c730728d90cf57bc98e95
Choerul Sofyan Muhammad Falih <choerulsofyanmf@gmail.com>
2021-05-14 18:20:26 +0700

    Photo Gallery App

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -8,8 +8,8 @@
         </ion-tab-button>
           
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+          <ion-icon :icon="images" />
+          <ion-label>Photos</ion-label>
         </ion-tab-button>
         
         <ion-tab-button tab="tab3" href="/tabs/tab3">
@@ -23,13 +23,14 @@
 
 <script lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { ellipse, square, triangle, images } from 'ionicons/icons';
 
 export default {
   name: 'Tabs',
   components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
   setup() {
     return {
+      images,
       ellipse, 
       square, 
       triangle,
